2a8d00c66477ddc20fe0cc192cd4df3f8b712465
Kirilll96 <DvornikovKirilll@mail.ru>
2022-05-31 16:38:32 +0300

    change lesson 1!

--- Lesson courses/Lesson 1/Lesson 1.py
+++ Lesson courses/Lesson 1/Lesson 1.py
@@ -1,10 +1,22 @@
 import os
+from re import S
 import pandas
 
 
+def summ(number, number2):
 
-i = 1
-q = 5
-summ = i + q
-print(summ)
-print("Hello world")
+    number = 1.655
+    number2 = 5.588
+    summ = number + number2
+    return summ
+def f(x) -> 123:
+    return x
+
+
+def main():
+    sum(12, 24)
+    f(1)
+
+
+if __name__ == "_main___":
+    main()
