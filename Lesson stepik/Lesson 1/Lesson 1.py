ac42498c48710a74333bf0f6d51d4a080f2bffd6
Kirilll96 <DvornikovKirilll@mail.ru>
2022-06-01 16:00:59 +0300

    Complite_lesson_1

--- Lesson stepik/Lesson 1/Lesson 1.py
+++ Lesson stepik/Lesson 1/Lesson 1.py
@@ -1,22 +1,12 @@
-import os
-from re import S
-import pandas
+n = int(input("Введите число последовательности: "))
+summ = 0
 
 
-def summ(number, number2):
 
-    number = 1.655
-    number2 = 5.588
-    summ = number + number2
-    return summ
-def f(x) -> 123:
-    return x
-
-
-def main():
-    sum(12, 24)
-    f(1)
-
-
-if __name__ == "_main___":
-    main()
+if(1 <= n and n <= 100):
+    for i in range(1, n + 1):
+        inp_val = int(input(f"Введите число #{i}: "))
+        summ += inp_val
+else:
+    print("Введеное число последовательности бльшое или слишком мало")
+print("Сумма чисел = ", summ)
